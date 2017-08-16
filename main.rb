8d9900ed87da3170fd3b1e8c4fb635f6b2d8ac1a
sksat <sksat.tec@gmail.com>
2017-08-16 17:32:14 +0900

    [ADD] 千種夜羽に関するツイートの監視

--- main.rb
+++ main.rb
@@ -1,4 +1,21 @@
 #!/usr/bin/ruby
 load 'client.rb'
 
-tc = TwitterClient.new
+twitter_client = TwitterClient.new
+
+tc = twitter_client.getClient
+
+id = ""
+while true do
+	tweet = tc.search("千種夜羽").first
+	if tweet.id != id
+		if tweet.user.id != 876273884563030018
+			user = tweet.user
+			str = "@sksat_tty " + user.name + "(" + user.uri + ")さんが千種夜羽についてツイートしています"
+			puts str
+			tc.update str
+		end
+	end
+	id = tweet.id
+	sleep 30
+end

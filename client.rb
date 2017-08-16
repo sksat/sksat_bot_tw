5405c5c5a0c1c9616b7af1c30bf6e2bb7e4e7c5e
sksat <sksat.tec@gmail.com>
2017-08-16 17:30:51 +0900

    [ADD] stream

--- client.rb
+++ client.rb
@@ -12,6 +12,20 @@ class TwitterClient
 			config.access_token			= @keys["ACCESS_TOKEN"]
 			config.access_token_secret	= @keys["ACCESS_TOKEN_SECRET"]
 		end
+		@stream = Twitter::Streaming::Client.new do |config|
+			config.consumer_key         = @keys["CONSUMER_KEY"]
+ 			config.consumer_secret      = @keys["CONSUMER_SECRET"]
+ 			config.access_token         = @keys["ACCESS_TOKEN"]
+ 			config.access_token_secret  = @keys["ACCESS_TOKEN_SECRET"]
+		end
+	end
+
+	def getClient
+		return @client
+	end
+
+	def getStream
+		return @stream
 	end
 
 	def tweet(str)
@@ -20,6 +34,10 @@ class TwitterClient
 
 	def reply(status_id, str)
 		@client.update(str, in_reply_to_status_id: status_id)
+	end
+
+	def search(str)
+		return @client.search(str, :count => 10, :result_type => "recent")
 	end
 end
 
